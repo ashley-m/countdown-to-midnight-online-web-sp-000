a8871379815a193e77b1bd1c410ac8659d8551fd
Ashley Miller <asm2fe@virginia.edu>
2019-03-27 22:33:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,14 @@ def countdown(iter)
   end
 return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(iter)
+  x=iter
+  iter.times do
+    puts "#{x} SECOND(S)!"
+    x-=1
+    sleep(1)
+  end
+return "HAPPY NEW YEAR!"
+  
+end
